f40c6d0368c78d6a73495ad5a6c1e8dfc6048d3b
kishore kumar reddy nerkundram gajendran <kn272@afsconnect1.njit.edu>
2017-11-02 21:29:16 -0400

    added PDO object

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
+  $hostname = "sql.njit.edu";
+  $username = "kn272";
+  $password = "KYhRX7n1z";
+  $conn = NULL;
 
+  try{
+     $conn = new PDO("mysql:host=$hostname;dbname=kn272",$username,$password);
+     echo "Connected successfully<br>";
+  }
+  catch(PDOException $e) {
+     http_error("500 Internal Server Error\n\n" . "There was a SQL error:\n\n" . $e->getMessage() . "<br>");
+  }
 ?>
