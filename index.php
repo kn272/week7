e255c8cd9e421bb9317ac7a00e1bf43dcf36df1d
kishore kumar reddy nerkundram gajendran <kn272@afsconnect1.njit.edu>
2017-11-03 00:18:02 -0400

    no. of records thing added

--- index.php
+++ index.php
@@ -4,11 +4,28 @@
   $password = "KYhRX7n1z";
   $conn = NULL;
 
-  try{
+  try {
      $conn = new PDO("mysql:host=$hostname;dbname=kn272",$username,$password);
      echo "Connected successfully<br>";
+     echo count(select()) . 'records have id < 6<br>';
   }
   catch(PDOException $e) {
      http_error("500 Internal Server Error\n\n" . "There was a SQL error:\n\n" . $e->getMessage() . "<br>");
   }
+
+  function select(){
+     global $conn;
+     try {
+        $select = 'SELECT * FROM accounts WHERE `id` < 6';
+	$sql = $conn->prepare($select);
+        $sql->execute();
+	$results = $sql->fetchAll();
+	$sql->closeCursor();
+	return $results;
+     }
+     catch(PDOException $e) {
+        http_error("500 Internal Server Error\n\n" . "There was a SQL error:\n\n" . $e->getMessage() . "<br>");
+     }
+  }
+
 ?>
