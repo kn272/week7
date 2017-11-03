08f8452784d80c95cef46160536e5402f77b4698
kishore kumar reddy nerkundram gajendran <kn272@afsconnect1.njit.edu>
2017-11-03 02:16:45 -0400

    added table class

--- index.php
+++ index.php
@@ -7,25 +7,53 @@
   try {
      $conn = new PDO("mysql:host=$hostname;dbname=kn272",$username,$password);
      echo "Connected successfully<br>";
-     echo count(select()) . 'records have id < 6<br>';
+     $select = 'SELECT * FROM accounts WHERE `id` < 6';
+     $rows = execQuery($select);
+     echo count($rows) . ' records have id < 6<br>';
+     $header = 'SHOW COLUMNS FROM accounts';
+     $heading = execQuery($header);
+     echo table($heading,$rows);     
   }
   catch(PDOException $e) {
      http_error("500 Internal Server Error\n\n" . "There was a SQL error:\n\n" . $e->getMessage() . "<br>");
   }
 
-  function select(){
+  function execQuery($query){
      global $conn;
      try {
-        $select = 'SELECT * FROM accounts WHERE `id` < 6';
-	$sql = $conn->prepare($select);
+	$sql = $conn->prepare($query);
         $sql->execute();
-	$results = $sql->fetchAll();
+	$results = $sql->fetchAll(PDO::FETCH_ASSOC);
 	$sql->closeCursor();
 	return $results;
      }
      catch(PDOException $e) {
         http_error("500 Internal Server Error\n\n" . "There was a SQL error:\n\n" . $e->getMessage() . "<br>");
      }
+  }
+
+  function table($heading,$rows) {
+     //print_r($heading);
+     //echo '<br>';
+     $table = NULL;
+     $table .= "<table border = 1>";
+     /*foreach ($heading as $row) {
+        $table .= "<th>";
+	foreach ($row as $column) {
+           $table .= "<td>$column[Field]</td>";
+	}
+	$table .= "</th>";
+
+     }*/ 
+     foreach ($rows as $row) {
+        $table .= "<tr>";
+        foreach ($row as $column) {
+           $table .= "<td>$column</td>";
+        }
+        $table .= "</tr>";
+     }
+     $table .= "</table>";
+     return $table;
   }
 
 ?>
